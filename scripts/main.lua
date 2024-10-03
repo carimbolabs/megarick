44960036668b179bd1d83b05924079b584e3b972
Rodrigo Delduca <rodrigo@delduca.org>
2024-10-03 09:31:00 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,21 +1,25 @@
 local engine = EngineFactory.new()
     :set_title("Mega Rick")
-    :set_width(854)
-    :set_height(480)
+    :set_width(1280)
+    :set_height(720)
     :set_fullscreen(false)
     :create()
 
 engine:prefetch({
-  "blobs/spritesheet.png",
+  "blobs/player.png",
 })
 
 
 local player = engine:spawn("player")
+player:set_action("run")
+player:set_placement(0, 580)
 
--- mr:on_update(function(self)
---   print("bump")
--- end)
+local function loop(delta)
+  print("Lua loop function called with delta: " .. delta)
+end
 
--- local head = engine:spawn()
+print(type(loopable_proxy))
+
+-- engine.add_loopable(loopable_proxy(loop))
 
 engine:run()
