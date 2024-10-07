2d25dd6909576bc39bb9dc5965b337e5dce3c4f6
Rodrigo Delduca <rodrigo@delduca.org>
2024-10-07 09:21:08 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -23,63 +23,16 @@ local postal = PostalService.new()
 
 local soundmanager = engine:soundmanager()
 
-local bullets = {}
-
-local state = {
-  space = false
-}
-
 local octopus = engine:spawn("octopus")
-local life = 13
-
-octopus:set_action("idle")
-octopus:set_placement(1200, 620)
-octopus:on_mail(function(self, message)
-  if message == 'hit' then
-    octopus:set_action("attack")
-    life = life - 1
-    if life <= 0 then
-      self:set_action("dead")
-    else
-      -- local explosion = engine:spawn("explosion")
-      -- explosion:set_action("explosion")
-      -- explosion:set_placement(1300, 700)
-    end
-  end
-end)
-
 local player = engine:spawn("player")
-player:set_action("idle")
-player:set_placement(30, 794)
-
 local princess = engine:spawn("princess")
-princess:set_action("idle")
-princess:set_placement(1600, 806)
-
 local candle1 = engine:spawn("candle")
-candle1:set_action("light")
-candle1:set_placement(60, 100)
-
 local candle2 = engine:spawn("candle")
-candle2:set_action("light")
-candle2:set_placement(1800, 100)
 
--- local explosion = engine:spawn("explosion")
--- explosion:set_action("explosion")
--- explosion:set_placement(1300, 700)
+local life = 20
 
--- local bullet = engine:spawn("bullet")
--- bullet:set_action("shoot")
--- bullet:set_placement(300, 580)
--- bullet:set_velocity(Vector2D.new(0.4, 0))
--- bullet:on_update(function(self)
---   if self.x > 860 then
---     local message = Mail.new(0, "hit")
---     postal:post(message)
---     engine:destroy(bullet)
---   end
--- end)
 local bullet_pool = {}
+local explosion_pool = {}
 
 local function create_bullet_pool(size)
   for _ = 1, size do
@@ -88,7 +41,6 @@ local function create_bullet_pool(size)
 
     bullet:on_update(function(self)
       if self.x > 1200 then
-        print("self x " .. self.x)
         local message = Mail.new(0, "bullet", "hit")
         postal:post(message)
 
@@ -102,6 +54,63 @@ local function create_bullet_pool(size)
   end
 end
 
+create_bullet_pool(3)
+
+local function create_explosion_pool(size)
+  for _ = 1, size do
+    local explosion = engine:spawn("explosion")
+    explosion:set_placement(-128, -128)
+    table.insert(explosion_pool, explosion)
+  end
+end
+
+create_explosion_pool(32)
+
+local function bomb()
+  print("pool size " .. #explosion_pool)
+  if #explosion_pool > 0 then
+    local explosion = table.remove(explosion_pool)
+    local x, y = octopus.x, player.y
+    local offset_x = (math.random(-2, 2)) * 30
+    local offset_y = (math.random(-2, 2)) * 30
+
+    explosion:set_placement(x + offset_x, y + offset_y)
+    explosion:set_action("default")
+    explosion:on_update(function(self)
+      if not self.visible then
+        self:set_placement(-128, -128)
+      end
+    end)
+  end
+end
+
+octopus:set_action("idle")
+octopus:set_placement(1200, 620)
+octopus:on_mail(function(self, message)
+  if message == 'hit' then
+    bomb()
+
+    octopus:set_action("attack")
+
+    life = life - 1
+    if life <= 0 then
+      self:set_action("dead")
+    end
+  end
+end)
+
+player:set_action("idle")
+player:set_placement(30, 794)
+
+princess:set_action("default")
+princess:set_placement(1600, 806)
+
+candle1:set_action("default")
+candle1:set_placement(60, 100)
+
+candle2:set_action("default")
+candle2:set_placement(1800, 100)
+
 local function fire()
   if #bullet_pool > 0 then
     local bullet = table.remove(bullet_pool)
@@ -110,34 +119,31 @@ local function fire()
 
     bullet:set_placement(x, y + offset_y)
     bullet:set_velocity(Vector2D.new(0.6, 0))
-    bullet:set_action("shoot")
+    bullet:set_action("default")
 
     local sound = "bomb" .. math.random(1, 2)
     soundmanager:play(sound)
   end
 end
 
-create_bullet_pool(3)
-
+local shooting = false
 
 player:on_update(function(self)
   local velocity = Vector2D.new(0, 0)
 
-  if engine:is_keydown(KeyEvent.a) then
-    velocity.x = -.4
-    -- octopus:set_action("attack")
-  elseif engine:is_keydown(KeyEvent.d) then
-    velocity.x = .4
-    -- octopus:set_action("dead")
-  end
-
   if engine:is_keydown(KeyEvent.space) then
-    if not state.space then
+    if not shooting then
       fire()
-      state.space = true
+      shooting = true
     end
   else
-    state.space = false
+    shooting = false
+  end
+
+  if engine:is_keydown(KeyEvent.a) then
+    velocity.x = -.4
+  elseif engine:is_keydown(KeyEvent.d) then
+    velocity.x = .4
   end
 
   if velocity:moving() then
