2b7664bc58395607300361a274a9754829530d08
Rodrigo Delduca <rodrigo@delduca.org>
2024-11-19 18:18:03 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -44,7 +44,7 @@ for _ = 1, 3 do
     if self.x > 1200 then
       postal:post(Mail.new(0, "bullet", "hit"))
       bullet:unset_action()
-      bullet:set_placement(-128, -128)
+      bullet:set_placement(-1024, -1024)
       table.insert(bullet_pool, bullet)
     end
   end)
@@ -53,7 +53,7 @@ end
 
 for _ = 1, 9 do
   local explosion = engine:spawn("explosion")
-  explosion:set_placement(-128, -128)
+  explosion:set_placement(-1024, -1024)
   table.insert(explosion_pool, explosion)
 end
 
@@ -69,7 +69,7 @@ local function bomb()
     explosion:set_action("default")
     explosion:on_animationfinished(function(self)
       self:unset_action()
-      self:set_placement(-128, -128)
+      self:set_placement(-1024, -1024)
       table.insert(explosion_pool, self)
     end)
   end
@@ -78,7 +78,7 @@ end
 local timer = false
 
 octopus:set_action("idle")
-octopus:set_placement(1200, 620)
+octopus:set_placement(1200, 622)
 octopus:on_mail(function(self, message)
   if message == 'hit' then
     bomb()
@@ -117,7 +117,7 @@ octopus:on_mail(function(self, message)
 end)
 
 player:set_action("idle")
-player:set_placement(30, 0)
+player:set_placement(30, 794)
 
 princess:set_action("default")
 princess:set_placement(1600, 806)
@@ -128,44 +128,42 @@ candle1:set_placement(60, 100)
 candle2:set_action("default")
 candle2:set_placement(1800, 100)
 
-floor:set_placement(0, 10000)
+floor:set_placement(-16192, 923)
 
 local function fire()
-  if #bullet_pool > 0 then
-    local bullet = table.remove(bullet_pool)
-    local x = (player.x + player.size.width) - 30
-    local y = player.y + 30
-    local offset_y = (math.random(-2, 2)) * 20
+  print("jump!")
+  -- if #bullet_pool > 0 then
+  --   local bullet = table.remove(bullet_pool)
+  --   local x = (player.x + player.size.width) - 30
+  --   local y = player.y + 30
+  --   local offset_y = (math.random(-2, 2)) * 20
 
-    bullet:set_placement(x, y + offset_y)
-    bullet:set_velocity(Vector2D.new(0.6, 0))
-    bullet:set_action("default")
+  --   bullet:set_placement(x, y + offset_y)
+  --   -- bullet:set_velocity(Vector2D.new(0.6, 0))
+  --   bullet:set_action("default")
 
-    local sound = "bomb" .. math.random(1, 2)
-    soundmanager:play(sound)
-  end
+  --   local sound = "bomb" .. math.random(1, 2)
+  --   soundmanager:play(sound)
+  -- end
 end
 
 player:on_update(function(self)
-  local velocity = 0
-
-  if engine:is_keydown(KeyEvent.space) and not shooting then
-    fire()
-    shooting = true
-  elseif not engine:is_keydown(KeyEvent.space) then
-    shooting = false
-  end
-
   if engine:is_keydown(KeyEvent.a) then
-    velocity = -300
     self:set_flip(Flip.horizontal)
+    self:set_action("run")
+    self:move(-300, self.velocity.y)
   elseif engine:is_keydown(KeyEvent.d) then
-    velocity = 300
     self:set_flip(Flip.none)
+    self:set_action("run")
+    self:move(300, self.velocity.y)
+  else
+    self:set_action("idle")
+    self:move(0, self.velocity.y)
   end
 
-  self:set_action(velocity ~= 0 and "run" or "idle")
-  self:move(velocity)
+  if engine:is_keydown(KeyEvent.space) then -- and self.velocity.y == 0 then
+    self:move(self.velocity.x, -1000)
+  end
 end)
 
 engine:run()
