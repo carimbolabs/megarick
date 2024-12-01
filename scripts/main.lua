7e5d6ddb635d7a6107583baa3e0cb73792884188
Rodrigo Delduca <rodrigo@delduca.org>
2024-12-01 12:56:27 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,6 +1,5 @@
 local helpers = require("helpers")
 
-print(helpers)
 helpers.hello()
 
 local engine = EngineFactory.new()
@@ -12,6 +11,7 @@ local engine = EngineFactory.new()
     :with_gravity(980.2)
     :with_fullscreen(false)
     :create()
+
 
 local postal = PostalService.new()
 local timemanager = TimeManager.new()
@@ -35,6 +35,14 @@ resourcemanager:prefetch({
   "blobs/ship.png",
 })
 
+scenemanager:set("ship")
+
+local font = fontfactory:get("fixedsys")
+
+local label = overlay:create(WidgetType.label)
+label:set_font(font)
+label:set("Hello World", 20, 20)
+
 local candle1 = entitymanager:spawn("candle")
 local candle2 = entitymanager:spawn("candle")
 local octopus = entitymanager:spawn("octopus")
@@ -42,146 +50,146 @@ local player = entitymanager:spawn("player")
 local princess = entitymanager:spawn("princess")
 local floor = entitymanager:spawn("floor")
 
-scenemanager:set("ship")
+-- scenemanager:set("ship")
 
-local life = 20
-local shooting = false
-local bullet_pool = {}
-local explosion_pool = {}
+-- local life = 20
+-- local shooting = false
+-- local bullet_pool = {}
+-- local explosion_pool = {}
 
-for _ = 1, 3 do
-  local bullet = entitymanager:spawn("bullet")
-  bullet:set_placement(-128, -128)
-  bullet:on_update(function(self)
-    if self.x > 1200 then
-      postal:post(Mail.new(0, "bullet", "hit"))
-      bullet:unset_action()
-      bullet:set_placement(-1024, -1024)
-      table.insert(bullet_pool, bullet)
-    end
-  end)
-  table.insert(bullet_pool, bullet)
-end
-
-for _ = 1, 9 do
-  local explosion = entitymanager:spawn("explosion")
-  explosion:set_placement(-1024, -1024)
-  table.insert(explosion_pool, explosion)
-end
-
---
---
---
---
---
-
--- print("1")
--- local widget = overlay:create(WidgetType.label)
--- print("2")
--- print(widget)
--- print("3")
--- local label = to_label(widget)
--- print("4")
--- print(label)
--- print(5)
--- print(6)
--- print(font)
--- print(7)
---
-local font = fontfactory:get("fixedsys")
--- print("font")
--- print(font)
--- local label = Label.new()
--- label:set("Hello world!", 10, 10)
--- label:set_font(font)
--- overlay:add(label)
--- label:set("Rodrigo")
--- overlay:remove(label)
-
-local label = overlay:create(WidgetType.label)
-label:set_font(font)
-label:set("Hello World", 20, 20)
---overlay:destroy(label)
--- if label then
---   print("Label created successfully!")
---   label:set_font(fontfactory:get("fixedsys"))
---   label:set("Hello world!", 10, 10)
--- else
---   print("Failed to cast widget to label")
+-- for _ = 1, 3 do
+--   local bullet = entitymanager:spawn("bullet")
+--   bullet:set_placement(-128, -128)
+--   bullet:on_update(function(self)
+--     if self.x > 1200 then
+--       postal:post(Mail.new(0, "bullet", "hit"))
+--       bullet:unset_action()
+--       bullet:set_placement(-1024, -1024)
+--       table.insert(bullet_pool, bullet)
+--     end
+--   end)
+--   table.insert(bullet_pool, bullet)
 -- end
 
--- local label = to_label(overlay:create(WidgetType.label))
--- local font = fontfactory:get("fixedsys")
--- print(">>>>>>>>>>>")
--- print(label)
--- print(font)
--- print(">>>>>>>>>>>")
--- label:set_font(fontfactory:get("fixedsys"))
--- label:set("Hello world!", 10, 10)
---
---
---
---
---
+-- for _ = 1, 9 do
+--   local explosion = entitymanager:spawn("explosion")
+--   explosion:set_placement(-1024, -1024)
+--   table.insert(explosion_pool, explosion)
+-- end
 
-local function bomb()
-  if #explosion_pool > 0 then
-    local explosion = table.remove(explosion_pool)
-    local x = octopus.x
-    local y = player.y
-    local offset_x = (math.random(-2, 2)) * 30
-    local offset_y = (math.random(-2, 2)) * 30
+-- --
+-- --
+-- --
+-- --
+-- --
 
-    explosion:set_placement(x + offset_x, y + offset_y)
-    explosion:set_action("default")
-    explosion:on_animationfinished(function(self)
-      self:unset_action()
-      self:set_placement(-1024, -1024)
-      table.insert(explosion_pool, self)
-    end)
-  end
-end
+-- -- print("1")
+-- -- local widget = overlay:create(WidgetType.label)
+-- -- print("2")
+-- -- print(widget)
+-- -- print("3")
+-- -- local label = to_label(widget)
+-- -- print("4")
+-- -- print(label)
+-- -- print(5)
+-- -- print(6)
+-- -- print(font)
+-- -- print(7)
+-- --
+-- -- local font = fontfactory:get("fixedsys")
+-- -- print("font")
+-- -- print(font)
+-- -- local label = Label.new()
+-- -- label:set("Hello world!", 10, 10)
+-- -- label:set_font(font)
+-- -- overlay:add(label)
+-- -- label:set("Rodrigo")
+-- -- overlay:remove(label)
 
-local timer = false
+-- -- local label = overlay:create(WidgetType.label)
+-- -- label:set_font(font)
+-- -- label:set("Hello World", 20, 20)
+-- --overlay:destroy(label)
+-- -- if label then
+-- --   print("Label created successfully!")
+-- --   label:set_font(fontfactory:get("fixedsys"))
+-- --   label:set("Hello world!", 10, 10)
+-- -- else
+-- --   print("Failed to cast widget to label")
+-- -- end
+
+-- -- local label = to_label(overlay:create(WidgetType.label))
+-- -- local font = fontfactory:get("fixedsys")
+-- -- print(">>>>>>>>>>>")
+-- -- print(label)
+-- -- print(font)
+-- -- print(">>>>>>>>>>>")
+-- -- label:set_font(fontfactory:get("fixedsys"))
+-- -- label:set("Hello world!", 10, 10)
+-- --
+-- --
+-- --
+-- --
+-- --
+
+-- local function bomb()
+--   if #explosion_pool > 0 then
+--     local explosion = table.remove(explosion_pool)
+--     local x = octopus.x
+--     local y = player.y
+--     local offset_x = (math.random(-2, 2)) * 30
+--     local offset_y = (math.random(-2, 2)) * 30
+
+--     explosion:set_placement(x + offset_x, y + offset_y)
+--     explosion:set_action("default")
+--     explosion:on_animationfinished(function(self)
+--       self:unset_action()
+--       self:set_placement(-1024, -1024)
+--       table.insert(explosion_pool, self)
+--     end)
+--   end
+-- end
+
+-- local timer = false
 
 octopus:set_action("idle")
 octopus:set_placement(1200, 622)
-octopus:on_mail(function(self, message)
-  if message == 'hit' then
-    bomb()
-    octopus:set_action("attack")
-    life = life - 1
-    if life <= 0 then
-      self:set_action("dead")
+-- octopus:on_mail(function(self, message)
+--   if message == 'hit' then
+--     bomb()
+--     octopus:set_action("attack")
+--     life = life - 1
+--     if life <= 0 then
+--       self:set_action("dead")
 
-      if not timer then
-        timemanager:singleshot(3000, function()
-          local function destroy(pool)
-            for i = #pool, 1, -1 do
-              entitymanager:destroy(pool[i])
-              table.remove(pool, i)
-            end
-          end
+--       if not timer then
+--         timemanager:singleshot(3000, function()
+--           local function destroy(pool)
+--             for i = #pool, 1, -1 do
+--               entitymanager:destroy(pool[i])
+--               table.remove(pool, i)
+--             end
+--           end
 
-          destroy(bullet_pool)
-          destroy(explosion_pool)
+--           destroy(bullet_pool)
+--           destroy(explosion_pool)
 
-          entitymanager:destroy(octopus)
-          entitymanager:destroy(player)
-          entitymanager:destroy(princess)
-          entitymanager:destroy(candle1)
-          entitymanager:destroy(candle2)
+--           entitymanager:destroy(octopus)
+--           entitymanager:destroy(player)
+--           entitymanager:destroy(princess)
+--           entitymanager:destroy(candle1)
+--           entitymanager:destroy(candle2)
 
-          engine:flush()
-          engine:prefetch({ "blobs/gameover.png" })
-          engine:set_scene("gameover")
-        end)
+--           engine:flush()
+--           engine:prefetch({ "blobs/gameover.png" })
+--           engine:set_scene("gameover")
+--         end)
 
-        timer = true
-      end
-    end
-  end
-end)
+--         timer = true
+--       end
+--     end
+--   end
+-- end)
 
 player:set_action("idle")
 player:set_placement(30, 794)
@@ -200,22 +208,22 @@ floor:set_placement(-16192, 923)
 local sound = "bomb" .. math.random(1, 2)
 soundmanager:play(sound)
 
-local function fire()
-  print("jump!")
-  -- if #bullet_pool > 0 then
-  --   local bullet = table.remove(bullet_pool)
-  --   local x = (player.x + player.size.width) - 30
-  --   local y = player.y + 30
-  --   local offset_y = (math.random(-2, 2)) * 20
+-- local function fire()
+--   print("jump!")
+--   -- if #bullet_pool > 0 then
+--   --   local bullet = table.remove(bullet_pool)
+--   --   local x = (player.x + player.size.width) - 30
+--   --   local y = player.y + 30
+--   --   local offset_y = (math.random(-2, 2)) * 20
 
-  --   bullet:set_placement(x, y + offset_y)
-  --   -- bullet:set_velocity(Vector2D.new(0.6, 0))
-  --   bullet:set_action("default")
+--   --   bullet:set_placement(x, y + offset_y)
+--   --   -- bullet:set_velocity(Vector2D.new(0.6, 0))
+--   --   bullet:set_action("default")
 
-  local sound = "bomb" .. math.random(1, 2)
-  soundmanager:play(sound)
-  -- end
-end
+--   local sound = "bomb" .. math.random(1, 2)
+--   soundmanager:play(sound)
+--   -- end
+-- end
 
 player:on_update(function(self)
   if statemanager:is_keydown(KeyEvent.a) then
@@ -232,9 +240,11 @@ player:on_update(function(self)
   end
 
   if statemanager:is_keydown(KeyEvent.space) then -- and self.velocity.y == 0 then
-    fire()
+    -- fire()
     self:move(self.velocity.x, -1000)
   end
 end)
+
+-- engine:run()
 
 engine:run()
